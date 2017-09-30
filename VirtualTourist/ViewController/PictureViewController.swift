29dc37b214d692906c7eaff3bf2d80755eb970d2
nsutanto <nicholas.sutanto@gmail.com>
2017-09-29 21:00:32 -0400

    working on UI

---
+++ VirtualTourist/ViewController/PictureViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PictureViewController.swift
+//  VirtualTourist
+//
+//  Created by Nicholas Sutanto on 9/29/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
