b5b0a3d51e420052e766ae8902d18dc5fd1f48f5
nsutanto <nicholas.sutanto@gmail.com>
2017-09-29 23:00:25 -0400

    Adding PIN

--- VirtualTourist/ViewController/MapViewController.swift
+++ VirtualTourist/ViewController/MapViewController.swift
@@ -9,13 +9,65 @@
 import UIKit
 import MapKit
 
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+           
+        }
+    }
+    
+    func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool) {
+        if (fullyRendered) {
+            performUIUpdatesOnMain {
+                //self.loadingIndicator.stopAnimating()
+            }
+        }
+    }
+}
+
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK : Action
+    
+    @IBAction func onLongPressAction(_ sender: Any) {
+        let lpg = sender as? UILongPressGestureRecognizer
+        
+        let pressPoint = lpg?.location(in: mapView)
+        let pressCoordinate = mapView.convert(pressPoint!, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pressCoordinate
+        
+        mapView.addAnnotation(annotation)
     }
 }
 
