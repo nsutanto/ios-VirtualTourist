a7f725bc0363e207766157b06410f90695d918d2
nsutanto <nicholas.sutanto@gmail.com>
2017-09-30 15:55:42 -0400

    Get the edit button working

--- VirtualTourist/ViewController/MapViewController.swift
+++ VirtualTourist/ViewController/MapViewController.swift
@@ -51,12 +51,25 @@ extension MapViewController: MKMapViewDelegate {
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var labelDelete: UILabel!
+    @IBOutlet weak var buttonEdit: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     // MARK : Action
+    @IBAction func onEditAction(_ sender: Any) {
+        // TODO : shift map up
+        if (buttonEdit.title == "Edit") {
+            labelDelete.isHidden = false
+            buttonEdit.title = "Done"
+        }
+        else {
+            labelDelete.isHidden = true
+            buttonEdit.title = "Edit"
+        }
+    }
     
     @IBAction func onLongPressAction(_ sender: Any) {
         let lpg = sender as? UILongPressGestureRecognizer
