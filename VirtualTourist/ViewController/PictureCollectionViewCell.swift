2f7f3de67de8f96ccd6ac4e801108630266644ea
nsutanto <nicholas.sutanto@gmail.com>
2017-10-06 21:35:54 -0400

    Adding CollectionViewCell

---
+++ VirtualTourist/ViewController/PictureCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PictureCollectionView.swift
+//  VirtualTourist
+//
+//  Created by Nicholas Sutanto on 10/6/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+    //@IBOutlet weak var myLabel: UILabel!
+}
--- VirtualTourist/ViewController/PictureViewController.swift
+++ VirtualTourist/ViewController/PictureViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 import MapKit
 
+
+
+extension PictureViewController: UICollectionViewDataSource {
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return self.items.count
+        return 4
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath as IndexPath) as! PictureCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        //cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+}
+
+extension PictureViewController: UICollectionViewDelegate {
+    
+}
+
 extension PictureViewController: MKMapViewDelegate {
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -34,12 +61,14 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var buttonPictureAction: UIButton!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var annotation: MKAnnotation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        collectionView.delegate = self
         initMap()
     }
     
